370fec8b629d9ef91ce0526cfd64efd58661c81c
Erandir Junior <aefs12junior@gmail.com>
2018-06-07 19:39:14 -0300

    Carregando imagens com webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import { abc } from './myfile.js';
+import jslogo from './javascript.png';
+
+const imgElement = document.querySelector('#my-image');
+
+imgElement.setAttribute('src', jslogo);
 
 console.log('mensagem do index');
 
 abc();
+
+/*
+1 - instalação do webpack local: npm install --save-dev webpack
+2 - instalação do webpack-cli:npm install --save-dev webpack-cli
+3 - Rodando o projeto: npm run build
+
+*/
