8d0c94a33fdd1db0bf71c0baee534af0373b7e60
Erandir Junior <aefs12junior@gmail.com>
2018-06-08 12:20:02 -0300

    Separando arquivo css em produção

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,28 @@
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const prodConfig = {
-
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							/*options: {
+								modules: true,
+								sourceMap: true
+							}*/
+						}
+					]
+				})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('style-bundle.css')
+	]
 };
 
 module.exports = prodConfig;
